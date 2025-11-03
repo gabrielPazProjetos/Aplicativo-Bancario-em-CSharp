89ce6eec35e46f64b548ac6b97334034c5ebf506
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-11-03 18:55:26 -0300

    Update Extensions.cs

--- Extensions.cs
+++ Extensions.cs
@@ -1,1 +1,34 @@
+using System;
 
+namespace Classes_M2
+{
+    public static class BankCustomerExtensions
+    {
+        // Checks if the customer ID is valid (10 digits)
+        public static bool IsValidCustomerId(this BankCustomer customer)
+        {
+            return customer.CustomerId.Length == 10;
+        }
+
+        // Returns a greeting message for the customer
+        public static string GreetCustomer(this BankCustomer customer)
+        {
+            return $"Hello, {customer.ReturnFullName()}!";
+        }
+    }
+
+    public static class BankAccountExtensions
+    {
+        // Checks if the account is overdrawn
+        public static bool IsOverdrawn(this BankAccount account)
+        {
+            return account.Balance < 0;
+        }
+
+        // Checks if a specific amount can be withdrawn
+        public static bool CanWithdraw(this BankAccount account, double amount)
+        {
+            return account.Balance >= amount;
+        }
+    }
+}
